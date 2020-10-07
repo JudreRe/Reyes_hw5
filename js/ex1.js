aa26bacf89db4f6fb50eb14d65c32803366da5ce
Judre <Reyesjudre@gmail.com>
2020-10-06 18:31:28 -0700

    made changes

--- js/ex1.js
+++ js/ex1.js
@@ -1,4 +1,21 @@
-const circle = {
-    
+class Circle {
+    constructor(perimeter, area, diagonal) {
+      this.perimeter = perimeter;
+      this.diagonal = diagonal;
+      this.side = 2;
+    }
 
-}
+    findPerimeter(one) {
+        this.perimeter = side * 4;
+    }
+    findArea(two) {
+        this.area = side ^ 2;
+    }
+    findDiagnonal(three) {
+        this.diagonal = Math.sqrt(2 * side);
+    }
+    Describe() {
+        return `${this.perimeter}, ${this.area}, ${this.diagonal};
+    }
+
+console.log(side.describe());
