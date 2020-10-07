6c5bdde7e3326efcdd0d80a7d777c8df9a06d38c
Judre <Reyesjudre@gmail.com>
2020-10-06 18:03:18 -0700

    first commit

---
+++ js/ex3.js
@@ -0,0 +1,63 @@
+const studentList = [
+
+    {
+
+        firstName: "Allan",
+
+        lastName: "Able",
+
+        scores: [95, 85, 92, 98]
+
+    },
+
+    {
+
+        firstName: "Amy",
+
+        lastName: "Alexander",
+
+        scores: [80, 88, 100]
+
+    },
+
+    {
+
+        firstName: "Betty",
+
+        lastName: "Barns",
+
+        scores: [70, 80, 90, 100]
+
+    },
+
+    {
+
+        firstName: "Bob",
+
+        lastName: "Bones",
+
+        scores: [75, 85, 95, 85]
+
+    },
+
+    {
+
+        firstName: "Cindy",
+
+        lastName: "Chase",
+
+        scores: [95, 90, 92, 98]
+
+    },
+
+    {
+
+        firstName: "Charles",
+
+        lastName: "Chips",
+
+        scores: [88, 99, 90]
+
+    },
+
+];
